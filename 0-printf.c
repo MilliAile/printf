861772a3003302656fe4e6aa6ccc938e1cb1a4fb
MilliAile <millisima45@gmail.com>
2023-11-16 22:51:02 +0300

    update

--- 0-printf.c
+++ 0-printf.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include "main.h"
+#include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
 /**
@@ -10,8 +11,8 @@
 int _printf(const char *format, ...)
 {
 	int a = 0, b = 0, c = 0;
-	char ch;
 	char *s;
+	int *size;
 	va_list print;
 
 	va_start(print, format);
@@ -20,14 +21,15 @@ int _printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
-		{
-			format++;
+		{ format++;
 			switch (*format)
 			{
 				case 'c':
-					ch = va_arg(print, int);
-					write(1, &ch, sizeof(char));
+					size = malloc(sizeof(char));
+					*size = va_arg(print, int);
+					write(1, size, 1);
 					b++;
+					free(size);
 					break;
 				case 's':
 					s = va_arg(print, char*);
