ad8e01acbff2acd7cf105c6ae84d2230c030673b
MilliAile <millisima45@gmail.com>
2023-11-16 21:54:56 +0300

    update

--- str.c
+++ str.c
@@ -1,9 +1,15 @@
 #include "main.h"
 #include <unistd.h>
 #include <stddef.h>
+/**
+ * str - function to return char printed and print string
+ * @s: the string input from _printf
+ * Return: the number of characters printed
+ */
 int str(char *s)
 {
 	int c;
+
 	if (s == NULL)
 	{
 		write(1, "(null)", 6);
